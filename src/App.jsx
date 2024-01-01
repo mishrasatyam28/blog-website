771a2fa31f95fe338ef686c4c1fc34d14d372106
Satyam Mishra <satyamsumit8750@gmail.com>
2024-01-01 17:10:11 +0530

    All environment variable export through conf file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import conf from "./conf/conf";
 
 function App() {
+  console.log(conf.appwriteUrl);
   return (
     <>
       <h1>A blog app with appwrite</h1>
