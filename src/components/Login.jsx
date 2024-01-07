69d4842ab36b5ee072ff56ec2c3339167115b4f4
Satyam Mishra <satyamsumit8750@gmail.com>
2024-01-07 23:31:30 +0530

    working in Login jsx file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,13 +4,47 @@ import { login as auhtLogin } from "../store/authSlice";
 import { Button, Input, Logo } from "./index";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
+import authService from "../appwrite/auth";
 useDispatch;
 
 function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
 
-  return <div>Login</div>;
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(auhtLogin(userData));
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className=" mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
