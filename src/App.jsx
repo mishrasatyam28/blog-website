6942d1675128660d4cc765918d33959559434057
Satyam Mishra <satyamsumit8750@gmail.com>
2024-03-14 21:05:53 +0530

    working with redux and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch();
+        }
+      })
+      .finally();
+  }, []);
+
   return <h1>Hello</h1>;
 }
 
