0c83dd015b106ce7547c9312f2744d1ba8f624cb
Satyam Mishra <satyamsumit8750@gmail.com>
2024-03-15 11:57:33 +0530

    store  configure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -12,13 +14,24 @@ function App() {
       .getCurrentUser()
       .then((userData) => {
         if (userData) {
-          dispatch();
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
         }
       })
-      .finally();
+      .finally(() => setLoading(false));
   }, []);
 
-  return <h1>Hello</h1>;
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+
+        <main>{/* <Outlet/> */}</main>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
