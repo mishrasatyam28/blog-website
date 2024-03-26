e4387661f486e091ba049cc1f69b835f4a8b32ac
Satyam Mishra <satyamsumit8750@gmail.com>
2024-03-26 21:34:59 +0530

    change Some UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
   ];
 
   return (
-    <header className="py-3 shadow bg-gray-500">
+    <header className="py-3 shadow bg-black fixed w-full z-10 text-gray-500">
       <Content>
         <nav className="flex">
           <div className="mr-4">
@@ -51,7 +51,7 @@ function Header() {
                 <li key={item.name}>
                   <button
                     onClick={() => navigate(item.slug)}
-                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-green-500 hover:text-black rounded-full"
                   >
                     {item.name}
                   </button>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Logo({ width = "100px" }) {
-  return <div>BlogBite</div>;
+  return <div className="text-2xl font-bold ">BlogX.</div>;
 }
 
 export default Logo;
